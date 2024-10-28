bee3c5b80e867f1912385b1dfaddeaa1e04812d4
4l3X-13 <navarroalejandro051@gmail.com>
2024-10-28 10:24:52 +0100

    Ejercicio 5

--- src/Tema2/Tema2Ejercicio5.java
+++ src/Tema2/Tema2Ejercicio5.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 
 public class Tema2Ejercicio5 {
     public static void main(String[] args) {
-        double n1;
-        double n2;
-        double n3;
-        double media;
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Inserta el primer número:");
-        n1 = scan.nextInt();
-        System.out.println("Inserta el segundo número:");
-        n2 = scan.nextInt();
-        System.out.println("Inserta el tercer número:");
-        n3 = scan.nextInt();
-        media = ( n1 + n2 + n3 )/3;
-        System.out.println("La media de "+ n1 + n2 + n3 +"es: " + media);
-        scan.close();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el primer número: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Ingrese el segundo número: ");
+        double num2 = scanner.nextDouble();
+        System.out.print("Ingrese el tercer número: ");
+        double num3 = scanner.nextDouble();
+        double media = (num1 + num2 + num3) / 3;
+
+        System.out.printf("La media con decimales es: %.2f%n", media);
+
+        System.out.printf("La media sin decimales es: %.0f%n", media);
+
+        scanner.close();
     }
 }
