97085f69628955acfcd34ce810662116707c440e
4l3X-13 <navarroalejandro051@gmail.com>
2024-10-28 09:26:05 +0100

    Ejercicio 13

---
+++ src/Tema2/Tema2Ejercicio13.java
@@ -0,0 +1,17 @@
+package Tema2;
+
+import java.util.Scanner;
+
+public class Tema2Ejercicio13 {
+    public static void main(String[] args) {
+        int hora, minutos, segundos;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Inserta las horas que quieres: ");
+        hora = scan.nextInt();
+        System.out.println("Inserta los minutos que quieres: ");
+        minutos = scan.nextInt();
+        System.out.println("Inserta los segundos que quieres: ");
+        segundos = scan.nextInt();
+        if
+    }
+}
