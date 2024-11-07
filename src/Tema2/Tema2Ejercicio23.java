e9f8a63ae7db6af82a5074fe6ce7912edc51fad9
4l3X-13 <navarroalejandro051@gmail.com>
2024-11-07 12:02:42 +0100

    Ejercicio 23

---
+++ src/Tema2/Tema2Ejercicio23.java
@@ -0,0 +1,34 @@
+package Tema2;
+
+import java.util.Scanner;
+
+public class Tema2Ejercicio23 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+                 int suma = 0;
+                 int contador = 0;
+                 int numero;
+
+                 System.out.println("Introduce números enteros positivos (introduce un número negativo para terminar):");
+
+                while (true) {
+                    numero = scanner.nextInt();
+
+                    if (numero < 0) {
+                        break;
+                    }
+
+                    suma += numero;
+                    contador++;
+                }
+
+                if (contador > 0) {
+                    double media = (double) suma / contador;
+                    System.out.println("La media de los números introducidos es: " + media);
+                } else {
+                    System.out.println("No se han introducido números positivos.");
+                }
+
+                scanner.close();
+            }
+        }
