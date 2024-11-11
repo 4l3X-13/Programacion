839aad070f429eb324c47c6cef55ead17832fe0c
4l3X-13 <navarroalejandro051@gmail.com>
2024-11-11 10:18:05 +0100

    Ejercicio 30

--- src/Tema2/Tema2Ejercicio30.java
+++ src/Tema2/Tema2Ejercicio30.java
@@ -5,59 +5,82 @@ import java.util.Scanner;
 public class Tema2Ejercicio30 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int num1, num2 = 0;
-        boolean entra = true;
-        char oper;
+                int num1, num2;
+                boolean entra = true;
+                char oper;
 
-        System.out.println("Bienvenido a la calculadora. Introduce 'salir' en cualquier momento para finalizar.");
+                System.out.println("Bienvenido a la calculadora. Introduce 'salir' en cualquier momento para finalizar.");
 
-        System.out.print("Introduce el primer número: ");
-        num1 = scan.nextInt();
+                while (entra) {
+                    System.out.print("Introduce el primer número: ");
+                    if (scan.hasNextInt()) {
+                        num1 = scan.nextInt();
+                    } else {
+                        String input = scan.next();
+                        if (input.equalsIgnoreCase("salir")) {
+                            break;
+                        } else {
+                            System.out.println("Entrada no válida. Por favor, introduce un número.");
+                            continue;
+                        }
+                    }
 
-        System.out.print("Introduce el primer número: ");
-        num2 = scan.nextInt();
+                    System.out.print("Introduce el segundo número: ");
+                    if (scan.hasNextInt()) {
+                        num2 = scan.nextInt();
+                    } else {
+                        String input = scan.next();
+                        if (input.equalsIgnoreCase("salir")) {
+                            break;
+                        } else {
+                            System.out.println("Entrada no válida. Por favor, introduce un número.");
+                            continue;
+                        }
+                    }
 
-        System.out.println("Inserta la operación que deseas realizar: (+,-,*,/,% s");
-        oper=scan.next().charAt(0);
+                    System.out.println("Inserta la operación que deseas realizar: (+, -, *, /, %, s para salir)");
+                    oper = scan.next().charAt(0);
 
-        switch (oper){
-            case 's' -> {
-                entra = false;
+                    switch (oper) {
+                        case 's':
+                            entra = false;
+                            break;
+
+                        case '+':
+                            System.out.println("La suma es: " + (num1 + num2));
+                            break;
+
+                        case '-':
+                            System.out.println("La resta es: " + (num1 - num2));
+                            break;
+
+                        case '*':
+                            System.out.println("La multiplicación es: " + (num1 * num2));
+                            break;
+
+                        case '/':
+                            if (num2 != 0) {
+                                System.out.println("La división es: " + (num1 / num2));
+                            } else {
+                                System.out.println("Error: No se puede dividir por 0.");
+                            }
+                            break;
+
+                        case '%':
+                            if (num2 != 0) {
+                                System.out.println("El resto es: " + (num1 % num2));
+                            } else {
+                                System.out.println("Error: No se puede calcular el resto con 0.");
+                            }
+                            break;
+
+                        default:
+                            System.out.println("Operación no válida. Intenta de nuevo.");
+                            break;
+                    }
+                }
+
+                System.out.println("Gracias por usar la calculadora.");
+                scan.close();
             }
-
-            case '+' ->{
-                int suma;
-                suma = num1 + num2;
-                System.out.println("La suma es: "+ suma);
-            }
-
-            case '-' ->{
-                int resta;
-                resta = num1 - num2;
-                System.out.println("La resta es: "+ resta);
-            }
-
-            case'*'->{
-                int multiplicar;
-                multiplicar = num1 * num2;
-                System.out.println("La multiplicación es: "+ multiplicar);
-            }
-            case'/'->{
-                int dividir;
-                dividir = num1 / num2;
-                System.out.println("La división es: "+ dividir);
-            }
-
-            case'%'->{
-                int resto;
-                resto = num1 % num2;
-                System.out.println("El resto es: "+ resto);
-            }
-
-            case
-
-
         }
-    scan.close();
-    }
-}
