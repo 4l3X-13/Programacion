69af379cf9df75f036322726c9930016de9a36b1
4l3X-13 <navarroalejandro051@gmail.com>
2024-12-02 09:52:47 +0100

    Tema3Ejercicio5

--- src/Tema3/src/Tema3Ejercicio5.java
+++ src/Tema3/src/Tema3Ejercicio5.java
@@ -1,7 +1,30 @@
 package Tema3.src;
+import java.util.Scanner;
 
 public class Tema3Ejercicio5 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Introduce un número entero del 1 al 10: ");
+        int numero = scanner.nextInt();
+        int numentero = entero(numero);
+        scanner.close();
     }
+
+    public static int entero(int numero) {
+        if (numero >= 1 && numero <= 10) {
+            System.out.println("Tabla de multiplicar del " + numero + ":");
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(numero + " x " + i + " = " + (numero * i));
+            }
+        } else {
+            System.out.println("Por favor, introduce un número válido entre 1 y 10.");
+        }
+
+
+        return numero;
+    }
+
+
 }
+
