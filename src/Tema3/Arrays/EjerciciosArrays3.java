4c1ff32237b8d8cbffbd38cae1e8adb75fe1905c
4l3X-13 <navarroalejandro051@gmail.com>
2025-01-23 13:43:46 +0100

    .

--- src/Tema3/Arrays/EjerciciosArrays3.java
+++ src/Tema3/Arrays/EjerciciosArrays3.java
@@ -1,22 +1,30 @@
 package Tema3.Arrays;
 
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class EjerciciosArrays3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        //Inicializar las variables
+        Random random = new Random();
+        int posicionMosca = random.nextInt(16);
         System.out.println("JUEGO ATRAPA A LA MOSCA:");
-        System.out.println("Indica donde está la mosca: ");
-        int slecUsu = scanner.nextInt();
+        int selecUsu;
 
-        int [] posicionMosca = new int[slecUsu];
+        //Bucle que te dice si has acertado o no
+        do {
+            System.out.println("Indica donde está la mosca: ");
+            selecUsu = scanner.nextInt();
+            if (selecUsu == posicionMosca) {
 
-        System.out.println(posicionMosca);
+                System.out.println("¡Has acertado!");
+            }else{
+                System.out.println("¡No has acertado!");
+            }
 
-
-
-
+        }while(selecUsu != posicionMosca);
 
 
     }
