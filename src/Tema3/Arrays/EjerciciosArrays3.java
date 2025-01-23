3ec3d3b5ac71eb169682150101210d1148a425dc
4l3X-13 <navarroalejandro051@gmail.com>
2025-01-23 13:11:03 +0100

    .

---
+++ src/Tema3/Arrays/EjerciciosArrays3.java
@@ -0,0 +1,24 @@
+package Tema3.Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class EjerciciosArrays3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("JUEGO ATRAPA A LA MOSCA:");
+        System.out.println("Indica donde está la mosca: ");
+        int slecUsu = scanner.nextInt();
+
+        int [] posicionMosca = new int[slecUsu];
+
+        System.out.println(posicionMosca);
+
+
+
+
+
+
+    }
+
+}
