e7861a0aa29aaac77c7fc81c2bb3634c8031f68a
HiNavarro <160244167+4l3X-13@users.noreply.github.com>
2025-01-24 18:52:23 +0100

    .

--- src/Tema3/Strings/EjercicioString1.java
+++ src/Tema3/Strings/EjercicioString1.java
@@ -1,16 +1,18 @@
-package Tema3;
-
 import java.util.Scanner;
+
 public class EjercicioString1 {
     // MAIN
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Elige una opción: " +
-                " 1. Invertir y poner en mayúsculas" +
-                " 2. Contar vocales" +
-                " 3. Encontrar la palabra más larga" +
-                " 4. Contar ocurrencias");
+        System.out.println("Elige una opción: \n" +
+                " 1. Invertir y poner en mayúsculas\n" +
+                " 2. Contar vocales\n" +
+                " 3. Encontrar la palabra más larga\n" +
+                " 4. Contar ocurrencias\n" +
+                " 5. Contar palabras\n" +
+                " 6. Formatear número de teléfono\n" +
+                " 7. Mostrar histograma de vocales");
         int eleccion = scanner.nextInt();
         scanner.nextLine(); // Consumir el salto de línea
 
@@ -36,38 +38,40 @@ public class EjercicioString1 {
                 int ocurrencias = contarOcurrencias(texto, cadenaABuscar);
                 System.out.println("Número de ocurrencias: " + ocurrencias);
                 break;
-
             case 5:
                 int numeroPalabras = contarPalabras(texto);
                 System.out.println("Número de palabras: " + numeroPalabras);
                 break;
-
-
-
+            case 6:
+                System.out.print("Ingresa el número de teléfono: ");
+                String numeroTelefono = scanner.nextLine();
+                String telefonoFormateado = formatearTelefono(numeroTelefono);
+                System.out.println("Número de teléfono formateado: " + telefonoFormateado);
+                break;
+            case 7:
+                mostrarHistogramaVocales(texto);
+                break;
             default:
                 System.out.println("Opción inválida.");
         }
     }
 
-
-    //a) Función que invierte la cadena y la convierte en mayúsculas
-    public static String invertirYmayusculas (String cadena){
+    // a) Función que invierte la cadena y la convierte en mayúsculas
+    public static String invertirYmayusculas(String cadena) {
         return new StringBuilder(cadena).reverse().toString().toUpperCase();
     }
 
-
-    //b) Función que cuenta el número de vocales
-    public static int contarVocales(String cadena){
+    // b) Función que cuenta el número de vocales
+    public static int contarVocales(String cadena) {
         int contador = 0;
         String vocales = "aeiouAEIOU";
         for (char c : cadena.toCharArray()) {
-            if(vocales.indexOf(c) != -1) {
+            if (vocales.indexOf(c) != -1) {
                 contador++;
             }
         }
         return contador;
     }
-
 
     // c) Función que encuentra la palabra más larga
     public static String palabraMasLarga(String cadena) {
@@ -82,15 +86,12 @@ public class EjercicioString1 {
         return palabraMasLarga;
     }
 
-//d) Función que recibe una cadena y devuelve el número de palabras que contiene.
+    // d) Función que cuenta las ocurrencias de una palabra
     public static int contarOcurrencias(String texto, String palabraBuscada) {
         texto = texto.toLowerCase();
         palabraBuscada = palabraBuscada.toLowerCase();
 
-        // Dividimos el texto en palabras (separando por espacios)
         String[] palabras = texto.split(" ");
-
-        // Contamos las ocurrencias
         int contador = 0;
         for (String palabra : palabras) {
             if (palabra.equals(palabraBuscada)) {
@@ -100,18 +101,41 @@ public class EjercicioString1 {
         return contador;
     }
 
-
-    //e) Función para contar palabras
+    // e) Función que cuenta el número de palabras
     public static int contarPalabras(String cadena) {
         String[] palabras = cadena.split(" ");
         return palabras.length;
     }
 
+    // f) Función que recibe un número de teléfono y lo convierte al formato (+34)-555-332211
+    public static String formatearTelefono(String numero) {
+        if (numero.length() < 11) {
+            return "Número inválido";
+        }
+        String codigoPais = "+".concat(numero.substring(0, 2));
+        String codigoArea = numero.substring(2, 5);
+        String restoNumero = numero.substring(5);
+        return String.format("(%s)-%s-%s", codigoPais, codigoArea, restoNumero);
+    }
 
-    //f) Función que recibe un número de teléfono y lo convierte en otro formato
+    // g) Procedimiento que muestra el histograma de frecuencias de las vocales
+    public static void mostrarHistogramaVocales(String cadena) {
+        int[] frecuencias = new int[5];
+        String vocales = "aeiou";
 
+        for (char c : cadena.toLowerCase().toCharArray()) {
+            int index = vocales.indexOf(c);
+            if (index != -1) {
+                frecuencias[index]++;
+            }
+        }
 
-
+        for (int i = 0; i < vocales.length(); i++) {
+            System.out.print(vocales.charAt(i) + " " + frecuencias[i] + " ");
+            for (int j = 0; j < frecuencias[i]; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
-
-
