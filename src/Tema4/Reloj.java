bd700869fe45a9ec2ee649d6958c84d00b4000c3
4l3X-13 <navarroalejandro051@gmail.com>
2025-01-30 13:42:05 +0100

    .

---
+++ src/Tema4/Reloj.java
@@ -0,0 +1,59 @@
+package Tema4;
+
+public class Reloj {
+    private int hora;
+    private int minuto;
+    private int segundo;
+    private boolean is24h;
+
+    public Reloj() {
+    }
+    public Reloj(int hora,int minuto,int segundo){
+
+    }
+
+    public int getHora() {
+        return hora;
+    }
+
+    public void setHora(int hora) {
+        if(hora < 0 && hora > 24){
+            System.out.println("Esta hora no es valida");
+        }
+        this.hora = hora;
+    }
+
+    public int getMinuto() {
+        return minuto;
+    }
+
+    public void setMinuto(int minuto) {
+       if (minuto < 0 && minuto > 60){
+           System.out.println("Este minuto no es valido");
+       }
+        this.minuto = minuto;
+    }
+
+    public int getSegundo() {
+        return segundo;
+    }
+
+    public void setSegundo(int segundo) {
+        if (segundo < 0 && segundo > 60){
+            System.out.println("Este segundo no es valido");
+        }
+        this.segundo = segundo;
+    }
+
+    public boolean isIs24h() {
+        return is24h;
+    }
+
+    public void setIs24h(boolean is24h) {
+        this.is24h = is24h;
+    }
+}
+
+
+
+
---
+++ src/Tema4/RelojMain.java
@@ -0,0 +1,11 @@
+package Tema4;
+
+public class RelojMain {
+    public static void main (String[] args) {
+
+        Reloj reloj1 = new Reloj();
+        Reloj reloj2 = new Reloj(23,59,59);
+        reloj2.setHora(0);
+    }
+
+}
