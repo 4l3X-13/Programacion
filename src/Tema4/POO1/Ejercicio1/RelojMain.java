4851149faca0722e2ec0d281b2fea8e0b3a6f3b0
HiNavarro <160244167+4l3X-13@users.noreply.github.com>
2025-02-11 16:34:37 +0100

    .

--- src/Tema4/POO1/Ejercicio1/RelojMain.java
+++ src/Tema4/POO1/Ejercicio1/RelojMain.java
@@ -1,11 +1,18 @@
-package Tema4;
-
 public class RelojMain {
-    public static void main (String[] args) {
-
+    public static void main(String[] args) {
         Reloj reloj1 = new Reloj();
-        Reloj reloj2 = new Reloj(23,59,59);
-        reloj2.setHora(0);
-    }
+        Reloj reloj2 = new Reloj(14, 30, 45);
 
+        reloj1.mostrarHora();
+        reloj2.mostrarHora();
+
+        reloj1.setHora(9);
+        reloj1.setMinuto(15);
+        reloj1.setSegundo(10);
+        reloj1.setFormato24h(false);
+
+        reloj1.mostrarHora();
+
+        System.out.println(reloj2);
+    }
 }
