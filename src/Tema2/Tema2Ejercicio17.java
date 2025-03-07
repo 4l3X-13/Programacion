a4ca2f2bf34dc22ad230249a3de138c884ac020e
4l3X-13 <navarroalejandro051@gmail.com>
2025-03-07 11:49:22 +0100

    .

--- src/Tema2/Tema2Ejercicio17.java
+++ src/Tema2/Tema2Ejercicio17.java
@@ -1,31 +1,4 @@
-package Tema2;
 
-import java.util.Scanner;
-
-public class Tema2Ejercicio17 {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-            int numero;
-            double cantidad, total;
-            System.out.println("Inserta la cantidad deseada: ");
-            cantidad = scan.nextInt();
-            System.out.println("Inserta D/E");
-            numero = scan.nextInt();
-
-            switch (numero){
-                case 0 -> {
-                    total = cantidad * 1.08;
-                    System.out.println("El total " + total + "€");
-                }
-
-                case 1 -> {
-                    total = cantidad * 0.92;
-                    System.out.println("La cantidad insertada son " + total + "$");
-                }
-            }
-            scan.close();
-        }
-    }
 
 
 
