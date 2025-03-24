c0d527e6aa71e014430bb9a8d70730f3a148f8b8
HiNavarro <160244167+4l3X-13@users.noreply.github.com>
2025-03-24 09:16:03 +0100

    Create Ejercicio1.java

---
+++ src/Tema 7/FicherosYserialización/Ejercicio1.java
@@ -0,0 +1,14 @@
+package tema7.FicherosYserialización;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inserta el nombre del usuario: ");
+        String nombre = scanner.next();
+        System.out.println("Inserta la edad del usuario");
+        int edad = scanner.nextInt();
+
+    }
+}
